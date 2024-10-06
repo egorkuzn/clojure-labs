2006ad653bb7ec290562bd0f9dbe5489bd5cf775
egorkuzn2002 <georkuzn2002@mail.ru>
2024-10-07 00:07:56 +0700

    lab-2: Started 2.2 task

--- labs/lab-2/src/lab_2/lab22.clj
+++ labs/lab-2/src/lab_2/lab22.clj
@@ -1,7 +1,29 @@
-(ns lab-2.lab22)
+(ns lab-2.lab22
+  (:require
+   [lab-2.lab21 :as lab21]))
+
+(defn delta-fun 
+  [f-t step h]
+  ())
+
+(defn lazy-integral-seq 
+  [f-t]
+  (iterate 
+   (fn [[step result]]
+     ([(inc step)
+       (+ result (delta-fun f-t step h))])) 
+   [0 0]))
 
 (defn lab-22
-  ([f-t x]
-   ()) 
-  ([f-t x h]
-   ()))
+  "Оптимизировано с помощью бесконечной последовательности частичных решений."
+  ([f-t x] (lab-22 f-t x 0.05))
+  ([f-t x h] (
+              (let [seq (lazy-integral-seq f-t)]
+                (fn [x]
+                  (nth seq (quot x h))))
+   )))
+
+(def fib (map
+          first 
+          (iterate (fn [[a b]] [b (+' a b)]) 
+                   [0 1])))
