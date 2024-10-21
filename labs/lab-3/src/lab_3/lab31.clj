4d10dcc87b0f5e9da4f1ff9a950d63e37ff50c5f
egorkuzn2002 <georkuzn2002@mail.ru>
2024-10-21 13:05:23 +0700

    lab-3: analize of the first and finishing second

--- labs/lab-3/src/lab_3/lab31.clj
+++ labs/lab-3/src/lab_3/lab31.clj
@@ -1,9 +1,12 @@
-(ns lab-3.lab31
-  (:require [clojure.math :as math]))
+(ns lab-3.lab31)
 
 (defn batch-count
   [coll batch-size]
-  (math/round (math/ceil (/ (count coll) batch-size))))
+  (let [n (count coll)]
+   (+ (/ n batch-size)
+     (if (> (mod n batch-size) 0) 1 0))))
+
+;; (println (time (batch-count (range 0 10000000) (/ 10000000 8))))
 
 (defn part-of
   [coll i batch-size]
@@ -15,8 +18,11 @@
   (let [n (batch-count coll batch-size)]
     (->> (iterate inc 0)
          (take n)
-         (map #(future (filter
-                        pred
-                        (part-of coll % batch-size))))
+         (map #(future (->>
+                        (filter
+                         pred
+                         (part-of coll % batch-size))
+                        (doall))))
          (doall)
-         (mapcat deref))))
+         (mapcat deref)
+         )))
--- labs/lab-3/src/lab_3/lab32.clj
+++ labs/lab-3/src/lab_3/lab32.clj
@@ -1,5 +1,10 @@
-(ns lab-3.lab32)
+(ns lab-3.lab32
+  (:require [lab-3.lab31 :refer :all]))
 
 (defn lab-32
-  []
-  ())
+  [pred coll batch-size]
+  (lazy-cat (lab-31 pred (take batch-size coll) batch-size)
+            (if (empty? coll)
+              coll
+              (lab-32 pred (drop batch-size coll) batch-size))
+            ))
