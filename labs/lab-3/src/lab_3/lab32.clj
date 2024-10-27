90c5c6c80f25780a447ca3d87c24a1690b496d28
egorkuzn2002 <georkuzn2002@mail.ru>
2024-10-27 22:29:01 +0700

    lab-3: tests works for first part

--- labs/lab-3/src/lab_3/lab32.clj
+++ labs/lab-3/src/lab_3/lab32.clj
@@ -3,8 +3,7 @@
 
 (defn lab-32
   [pred coll batch-size]
-  (lazy-cat (lab-31 pred (take batch-size coll) batch-size)
+  (lazy-cat (lab-31 pred (take batch-size coll) (/ batch-size))
             (if (empty? coll)
               coll
-              (lab-32 pred (drop batch-size coll) batch-size))
-            ))
+              (lab-32 pred (drop batch-size coll) batch-size))))
