3ed0e19f86aeb6df9dd458e59ae0f3e22cdf868c
egorkuzn2002 <georkuzn2002@mail.ru>
2024-10-28 12:31:06 +0700

    lab-4: separation of logic

---
+++ labs/lab-4/src/lab_4/variable.clj
@@ -0,0 +1,21 @@
+(ns lab-4.variable)
+
+(defn variable [name]
+  "создание переменной"
+  {:pre [keyword? name]}
+  (list ::variable name))
+
+(defn variable? [expr]
+  "проверка, переменная ли"
+  (= (first expr) ::variable))
+
+(defn variable-value [expr]
+  "получение значения переменной"
+  (second expr))
+
+(defn same-variables? [v1 v2]
+  "сравнение переменных"
+  (and
+   (variable? v1)
+   (variable? v2)
+   (= (second v1) (second v2))))
