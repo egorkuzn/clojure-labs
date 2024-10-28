d1d09cbab7e53ef43ef975dce7fe038bfd7a6f19
egorkuzn2002 <georkuzn2002@mail.ru>
2024-10-28 12:21:28 +0700

    lab-4: defining var and supporting its methods

--- labs/lab-4/src/lab_4/constants.clj
+++ labs/lab-4/src/lab_4/constants.clj
@@ -19,4 +19,23 @@
   "проверка, ложь ли"
   (and (constant? expr) (= (second expr) 0)))
 
+(defn variable [name]
+  "создание переменной"
+  {:pre [keyword? name]}
+  (list ::variable name))
 
+(defn variable? [expr]
+  "проверка, переменная ли"
+  (= (first expr) ::variable))
+
+(defn variable-value [expr]
+  "получение значения переменной"
+  (second expr))
+
+(defn same-variables? [v1 v2]
+  "сравнение переменных"
+  (and 
+   (variable? v1)
+   (variable? v2)
+   (= (second v1) (second v2))
+   ))
